7800d1da9fe1a131d9735f2a4ee4dfcb1abcb93b
Deepesh Pandey <124483889+Deepesh2425@users.noreply.github.com>
2026-09-11 14:38:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template, request, jsonify
 from pypdf import PdfReader
 from sklearn.feature_extraction.text import TfidfVectorizer
@@ -290,5 +291,8 @@ def analyze():
 
 # Start application
 if __name__ == "__main__":
-
-    app.run(debug=True)
+    app.run(
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 5000)),
+        debug=False
+    )
